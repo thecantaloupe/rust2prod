8dd4ea6012b2c09487b52b5280ac2c046fca71c0
Zachary Gomez <zgomez301@gmail.com>
2022-01-17 23:41:06 -0800

    chapter 2

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::net::TcpListener;
+
+use rust2prod_api::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    run(listener)?.await
+}
